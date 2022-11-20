80a34d1c1a54ea08317ee3b63324c57c75531abc
rainey1 <619701626@qq.com>
2022-11-20 16:19:12 +0800

    Site updated: 2022-11-20 16:19:11

--- js/runtime.js
+++ js/runtime.js
@@ -1,6 +1,6 @@
 var now=new Date;
 function createtime(){now.setTime(now.getTime()+1e3);
-    var e=new Date("08/01/2022 00:00:00"),t=Math.trunc(234e8+(now-e)/1e3*17),a=(t/1496e5).toFixed(6),n=new Date("20/10/2022 00:00:00"),s=(now-n)/1e3/60/60/24,o=Math.floor(s),r=(now-n)/1e3/60/60-24*o,i=Math.floor(r);
+    var e=new Date("08/01/2022 00:00:00"),t=Math.trunc(234e8+(now-e)/1e3*17),a=(t/1496e5).toFixed(6),n=new Date("08/09/2022 00:00:00"),s=(now-n)/1e3/60/60/24,o=Math.floor(s),r=(now-n)/1e3/60/60-24*o,i=Math.floor(r);
     1==String(i).length&&(i="0"+i);
     var l=(now-n)/1e3/60-1440*o-60*i,b=Math.floor(l);1==String(b).length&&(b="0"+b);
     var d=(now-n)/1e3-86400*o-3600*i-60*b,c=Math.round(d);
