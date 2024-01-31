a7644107f02f993862394b94dbc847a103dfe6dc
1chuci <619701626@qq.com>
2024-01-31 16:31:11 +0800

    Site updated: 2024-01-31 16:31:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/20/resurrection/","2024/01/19/hello-world/"];function toRandomPost(){
+var posts=["2024/01/24/work1/","2024/01/20/resurrection/","2024/01/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
